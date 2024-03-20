937fcfa63443ca4569902cb1603e61b983e628a5
nrennie <nrennie35@gmail.com>
2024-03-20 22:15:34 +0000

    Initializing gh-pages branch

---
+++ examples/Example 2/app.R
@@ -0,0 +1,38 @@
+library(shiny)
+library(palmerpenguins)
+
+# Penguin types
+species_choices <- unique(penguins$species)
+
+# UI ----------------------------------------------------------------------
+
+ui <- fluidPage(
+  titlePanel("Penguins!"),
+  sidebarLayout(
+
+    # Sidebar with user inputs
+    sidebarPanel(
+      # Select variable for plot
+      selectInput(
+        inputId = "species",
+        label = "Choose a species:",
+        choices = species_choices
+      )
+    ),
+
+    # Display a plot of the generated distribution
+    mainPanel()
+  )
+)
+
+
+# Server ------------------------------------------------------------------
+
+server <- function(input, output) {
+
+}
+
+
+# Run app -----------------------------------------------------------------
+
+shiny::shinyApp(ui, server)
